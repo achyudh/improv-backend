afab422c93c5b695e9d562aeddde44ab71011616
Achyudh K Ram <achyudhk@gmail.com>
2017-11-01 12:44:16 +0100

    Add util functions for IO

--- improv.py
+++ improv.py
@@ -1,19 +1,79 @@
 
 from sanic import Sanic
-from sanic.response import json
+from sanic import response
+from jwt import jwk_from_pem
+from requests.auth import HTTPBasicAuth
+from util import io
+import requests, json, urllib, pymongo, sys, secrets
 
 app = Sanic()
+with open("config.json", 'r') as config_file:
+    client_config = json.load(config_file)
+with open("private-key.pem", 'rb') as priv_key_file:
+    priv_key = jwk_from_pem(priv_key_file.read())
+http_auth_username = client_config['HTTP_AUTH_USERNAME']
+http_auth_secret = client_config['HTTP_AUTH_SECRET']
+http_auth = HTTPBasicAuth(http_auth_username, http_auth_secret)
+app.config['SESSION_TYPE'] = 'mongodb'
+app.config['GITHUB_CLIENT_ID'] = client_config['GITHUB_CLIENT_ID']
+app.config['GITHUB_CLIENT_SECRET'] = client_config['GITHUB_CLIENT_SECRET']
 
 
 @app.route("/webhook", methods=['POST'])
 async def test(request):
-    return json({"hello": "world"})
+    if request.json is None:
+        print("NULL REQUEST: " + request.headers, file=sys.stderr)
+        return response.json({}, status=400)
+
+    elif "action" not in request.json and request.headers['X-GitHub-Event'] == "ping":
+        # POST request has initial webhook and repo details
+        client = pymongo.MongoClient()
+        pr_db = client.im_database
+        # Insert init repo info into DB
+        pr_db.webhook_init.insert_one(request.json)
+        return response.json({}, status=200)
+
+    elif request.json.get("action", None) == "created" and request.headers['X-GitHub-Event'] == "installation":
+        client = pymongo.MongoClient()
+        pr_db = client.im_database
+        # Insert init repo info into DB
+        pr_db.app_init.insert_one(request.json)
+        return response.text('Install successful', status=200)
+
+    elif request.json.get("action", None) == "opened" and request.headers['X-GitHub-Event'] == "pull_request":
+        parsed_json = request.json
+        is_private_repo = request.json["pull_request"]["base"]["repo"]["private"]
+        pr_num = parsed_json["pull_request"]["number"]
+        pr_id = parsed_json["pull_request"]["id"]
+        repo_id = parsed_json["pull_request"]["base"]["repo"]["id"]
+
+
+
+        if "installation" in request.json:
+            pr_comment_url = 'https://api.github.com/repos/%s/issues/%s/comments' % (parsed_json["pull_request"]["base"]["repo"]["full_name"], pr_num)
+            pr_comment_payload = json.dumps({"body": "### Improv Reviewability Report"
+                                                     "\n\n*Major deterrents to the reviewability of this pull request:*\n"
+                                                     ":x: Factor One\n"
+                                                     "\n\n*Factors that might impair the reviewability of this pull request:*\n"
+                                                     ":large_orange_diamond: Factor Two\n"
+                                                     "\n\n*Factors that improve the reviewability of this pull request:*\n"
+                                                     ":heavy_check_mark: Factor Three\n"
+                                                     ":heavy_check_mark: Factor Four\n"
+                                             })
+            r = requests.post(pr_comment_url, data=pr_comment_payload, headers=io.get_auth_header(request.json["installation"]["id"], priv_key))
+        else:
+            return response.text("Authentication error", status=500)
+        if not is_private_repo:
+            io.download_patch(parsed_json["pull_request"]["patch_url"], http_auth, pr_id, repo_id)
+        return response.text("OK")
+    else:
+        return response.text("Request not handled", status=202)
 
 
 @app.route("/callback", methods=['POST'])
 def callback(request):
     client = pymongo.MongoClient()
-    pr_db = client.pr_database
+    pr_db = client.im_database
 
 
 if __name__ == "__main__":
---
+++ util/io.py
@@ -0,0 +1,31 @@
+import requests, time
+from jwt import JWT
+
+
+def download_patch(url, http_auth, pr_id, repo_id):
+    response_data = requests.get(url, auth=http_auth)
+    if response_data.status_code == 200:
+        with open('patches/%s-%s.txt' % (str(pr_id), str(repo_id)), 'wb') as f:
+            f.write(response_data.content)
+    else:
+        print("Error downloading patch: Status Code " + response_data.status_code)
+
+
+def get_auth_header(installation_id, priv_key):
+    payload = {"iss": 6166,
+               "iat": int(time.time()),
+               "exp": int(time.time()) + 300}
+    jwt = JWT()
+    token = jwt.encode(payload, priv_key, 'RS256')
+    # url = "https://api.github.com/app"
+    url = "https://api.github.com/installations/%s/access_tokens" % installation_id
+    headers = {'Accept': 'application/vnd.github.machine-man-preview+json',
+               'Authorization': 'Bearer ' + token}
+    r = requests.post(url, headers=headers)
+    ret_headers = {"Authorization": "token " + r.json()["token"],
+                   "Accept": "application/vnd.github.machine-man-preview+json"}
+    return ret_headers
+
+
+def get_pr_info():
+    
